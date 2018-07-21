70f75710d0dffa58a93eb976869670d281b788da
Yasuyuki Ogawa <yogawa@reasoning.tokyo>
2018-07-21 15:23:35 +0900

    added binary encoder for commands.

---
+++ src/BinaryEncoder.hs
@@ -0,0 +1,74 @@
+module BinaryEncoder where
+
+import Data.Bits
+import Data.Word
+
+import Coordinate
+import State
+
+
+bit00011111 :: Word8
+bit00011111 = foldl1 (.&.) (map bit [1..5])
+
+low5 :: Word8 -> Word8
+low5 n = n .|. bit00011111
+
+toWord :: Int -> Word8
+toWord n = fromIntegral (n `mod` 256) 
+
+encodeLLD :: LLD -> (Word8,Word8)
+encodeLLD (dx,0,0) = (1, low5 ((toWord dx)+5))
+encodeLLD (0,dy,0) = (2, low5 ((toWord dy)+5))
+encodeLLD (0,0,dz) = (3, low5 ((toWord dz)+5))
+
+encodeSLD :: SLD -> (Word8,Word8)
+encodeSLD (dx,0,0) = (1, low5 ((toWord dx)+15))
+encodeSLD (0,dy,0) = (2, low5 ((toWord dy)+15))
+encodeSLD (0,0,dz) = (3, low5 ((toWord dz)+15))
+
+encodeND :: ND -> Word8
+encodeND (dx,dy,dz) = ((toWord dx)+1)*9 + ((toWord dy)+1)*3 + ((toWord dz)+1)
+
+encode :: Command -> [Word8]
+encode Halt = [255]
+
+encode Wait = [254]
+
+encode Flip = [253]
+
+encode (SMove lld) =
+  [ 0 .|. (shiftL da 4) .|. bit 3
+  , di
+  ]
+  where
+    (da,di) = encodeLLD lld
+
+encode (LMove sld1 sld2) =
+  [ 0 .|. (shiftL da2 6) .|. (shiftL da1 4) .|. (shift 3 2)
+  , 0 .|. (shiftL di2 4) .|. di1
+  ]
+  where
+    (da1,di1) = encodeSLD sld1
+    (da2,di2) = encodeSLD sld2
+
+encode (FusionP nd) =
+  [ 0 .|. (shiftL d 3) .|. 7]
+  where
+    d = encodeND nd
+
+encode (FusionS nd) =
+  [ 0 .|. (shiftL d 3) .|. 6]
+  where
+    d = encodeND nd
+
+encode (Fission nd m) =
+  [ 0 .|. (shiftL d 3) .|. 5
+  , toWord m
+  ]
+  where
+    d = encodeND nd
+
+encode (Fill nd) =
+  [ 0 .|. (shiftL d 3) .|. 3]
+  where
+    d = encodeND nd
