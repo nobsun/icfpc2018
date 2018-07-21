ff0103f1820dd0325c23ec24ccea97051f52587e
Kei Hibino <ex8k.hibino@gmail.com>
2018-07-21 16:50:30 +0900

    implicitly map.

--- src/Matrix.hs
+++ src/Matrix.hs
@@ -31,8 +31,9 @@ isGrounded m =
     f (g,u) = (g `Set.union` s1, u Set.\\ s1)
       where
         s0 = Set.fromList
-             [c | Coord (x,y,z) <- Set.toList g
-                , c <- map Coord [(x-1,y,z),(x+1,y,z),(x,y-1,z),(x,y+1,z),(x,y,z-1),(x,y,z+1)]
+             [Coord c
+             | Coord (x,y,z) <- Set.toList g
+             , c <- [(x-1,y,z),(x+1,y,z),(x,y-1,z),(x,y+1,z),(x,y,z-1),(x,y,z+1)]
              ]
         s1 = Set.intersection s0 u
 
