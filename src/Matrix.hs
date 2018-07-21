ba6c402ba341a0109aaebdcc111a1a12ec177c73
Masahiro Sakai <masahiro.sakai@gmail.com>
2018-07-21 10:22:14 +0900

    add Matrix.hs

---
+++ src/Matrix.hs
@@ -0,0 +1,38 @@
+module Matrix where
+
+import Coordinate
+
+import Data.IntMap (IntMap)
+import qualified Data.IntMap as IntMap
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Set (Set)
+import qualified Data.Set as Set
+
+type Matrix = IntMap (Set (Int,Int))
+
+makeMatrix :: [Coord] -> Matrix
+makeMatrix cs = IntMap.fromListWith Set.union [(y, Set.singleton (x,z)) | (x,y,z) <- cs]
+
+matrixCoords :: Matrix -> [Coord]
+matrixCoords m = [(x,y,z) | (y, xzs) <- IntMap.toList m, (x,z) <- Set.toList xzs]
+
+isGrounded :: Matrix -> Bool
+isGrounded m = go (IntMap.toAscList m) (const True)
+  where
+    go :: [(Int, Set (Int,Int))] -> ((Int,Int) -> Bool) -> Bool
+    go [] _prev = True
+    go ((_y,xzs) : ls) prev = and table && go ls (\xz -> Map.lookup xz table == Just True)
+      where
+        table :: Map (Int,Int) Bool
+        table = converge f tbl0
+          where
+            tbl0 :: Map (Int,Int) Bool
+            tbl0 = Map.fromList [(xz, prev xz) | xz@(x,z) <- Set.toAscList xzs]
+            f :: Map (Int,Int) Bool -> Map (Int,Int) Bool
+            f tbl = Map.mapWithKey (\(x,z) g -> g || or [Map.lookup (x',z') tbl == Just True | (x',z') <- [(x-1,z),(x+1,z),(x,z-1),(x,z+1)]]) tbl
+        converge f x
+          | fx == x = x
+          | otherwise = converge f fx
+          where
+            fx = f x
