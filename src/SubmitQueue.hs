5bd54db76cf4372bacc355abf7b0f212290821fd
Kei Hibino <ex8k.hibino@gmail.com>
2018-07-23 02:04:32 +0900

    line buffering mode.

--- src/SubmitQueue.hs
+++ src/SubmitQueue.hs
@@ -13,6 +13,7 @@ import Data.Time
   (NominalDiffTime, diffUTCTime, getCurrentTime,
    FormatTime, formatTime)
 import Data.Time.Locale.Compat (defaultTimeLocale)
+import System.IO (stdout, BufferMode (LineBuffering), hSetBuffering)
 import System.FilePath ((</>), (<.>))
 import System.Directory (renameFile)
 import System.Process (rawSystem, readProcess)
@@ -23,6 +24,7 @@ import Path (submitExport)
 
 submitGateway :: IO ()
 submitGateway = do
+  hSetBuffering stdout LineBuffering
   putLog <- do
     put <- newLog
     return $ \s -> do
