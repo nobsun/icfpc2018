aac346c9d7450e1693e42c95d51845724df66511
Yasuyuki Ogawa <yogawa@reasoning.tokyo>
2018-07-23 05:17:47 +0900

    Naive bot supported re-assemble function.

--- app/oga.hs
+++ app/oga.hs
@@ -1,33 +1,45 @@
 import System.Environment
 
+import Data.List (sort)
+
 import TraceEncoder (writeTraceFile)
-import NaiveBot (getAssembleTrace', getDisassembleTrace')
+import NaiveBot (getAssembleTrace', getDisassembleTrace', getReassembleTrace')
 import Model (Model(..), readModel)
 import State (Trace, Command(..))
 
 main :: IO ()
 main = do
-  args <- getArgs
-  mapM_ f args
+  (flag:args) <- getArgs
+  case flag of
+    "FR" -> mapM_ solve2 (zip (sort args) (tail (sort args)))
+    _    -> mapM_ solve1 args
+
+solve1 :: String -> IO ()
+solve1 mdl = do
+  model <- readModel mdl
+  let trs = method model
+--  putStrLn ("resolution: " ++ show (mdResolution model))
+--  mapM_ print trs
+  writeTraceFile (mkNbtName mdl) (concat trs)
   where
-    f :: String -> IO ()
-    f mdl = do
-      model <- readModel mdl
-      let trs = (method mdl) model
---      putStrLn ("resolution: " ++ show (mdResolution model))
---      mapM_ print trs
-      writeTraceFile (mkNbtName mdl) (concat trs)
+    method = 
+      case take 2 mdl of
+        "FA" -> getAssembleTrace'
+        "FD" -> getDisassembleTrace'
+        _    -> const [[Halt]]
+
+solve2 :: (String, String) -> IO ()
+solve2 (src,tgt) = do
+  srcModel <- readModel src
+  tgtModel <- readModel tgt
+  let trs = getReassembleTrace' srcModel tgtModel
+--  putStrLn ("resolution: " ++ show (mdResolution srcModel))
+--  mapM_ print trs
+  writeTraceFile (mkNbtName src) (concat trs)
 
 mkNbtName :: String -> String
 mkNbtName str =
   name ++ ".nbt"
   where
     name = takeWhile (/='_') str
-
-method :: String -> Model -> [Trace]
-method fn =
-  case take 2 fn of
-    "FA" -> getAssembleTrace'
-    "FD" -> getDisassembleTrace'
-    _    -> const [[Halt]]
 
--- src/NaiveBot.hs
+++ src/NaiveBot.hs
@@ -5,10 +5,13 @@ module NaiveBot
   , getAssembleTrace'
   , getDisassembleTrace
   , getDisassembleTrace'
+  , getReassembleTrace
+  , getReassembleTrace'
   ) where
 
 import Control.Monad (replicateM)
 import Control.Monad.State.Lazy
+import Data.List (sort, sortBy)
 import qualified Data.IntMap as IntMap
 import qualified Data.IntSet as IntSet
 import qualified Data.Set as Set
@@ -17,7 +20,11 @@ import Coordinate
 import Model
 import State
 
-
+-------------------------------------------------------
+-- x軸を40分割してボットに割り当てる戦略.
+-- 自身のx軸の領域内を動くので, 他のボットと競合しない.
+-- 分裂するときは, 分裂先が横のボットの領域内になるようにし
+-- 逆に融合するときも, 横のボットと領域の境界越しに並ぶようにする.
 -------------------------------------------------------
 
 data B = B
@@ -83,22 +90,21 @@ sMoveAbs (x,y,z) = do
   replicateM (abs qz) (sMoveDz (15*(signum qz)))
   sMoveDz rz
 
-
-fillBottom :: BState ()
-fillBottom = do
+cFill :: ND -> BState ()
+cFill nd = do
   modify f
   where
     f :: B -> B
     f b@B{ bTrace = bTrace} =
-      b{ bTrace = [Fill (0,-1,0)]:bTrace }
+      b{ bTrace = [Fill nd]:bTrace }
 
-voidBottom :: BState ()
-voidBottom = do
+cVoid :: ND -> BState ()
+cVoid nd = do
   modify f
   where
     f :: B -> B
     f b@B{ bTrace = bTrace} =
-      b{ bTrace = [Void (0,-1,0)]:bTrace}
+      b{ bTrace = [Void nd]:bTrace}
 
 fissionX :: BState B
 fissionX = do
@@ -171,6 +177,19 @@ getDisassembleTrace' :: Model -> [Trace]
 getDisassembleTrace' =
   getTrace disassembleRoot
 
+getReassembleTrace :: Model -> Model -> Trace
+getReassembleTrace src tgt =
+  concat (getReassembleTrace' src tgt)
+
+getReassembleTrace' :: Model -> Model -> [Trace]
+getReassembleTrace' src@(Model r _) tgt =
+  reverse $ bTrace $ execState (reassembleRoot src tgt)
+  $ B{ bBot   = Bot{botPos=Coord(0,0,0), botId=1, botSeeds=IntSet.empty}
+     , bTrace = []
+     , bRange = (0, q+(if rest > 0 then 1 else 0))
+     }
+  where
+    (q,rest) = r`divMod`40 -- max 40 parallel
 
 -------------------------------------------------------
 
@@ -192,8 +211,17 @@ disassembleRoot md@(Model r _mat) = do
   sMoveAbs (0,0,0)
   cHalt
 
+reassembleRoot:: Model -> Model -> BState ()
+reassembleRoot src@(Model r _) tgt = do
+  cFlip
+  parallelReassemble src tgt
+  cFlip
+  sMoveAbs (0,r-1,0)
+  sMoveAbs (0,0,0)
+  cHalt
+
 parallelAssemble :: Model -> BState ()
-parallelAssemble md@(Model r mat) = do
+parallelAssemble md@(Model r _) = do
   B{bRange=(ra,rb)} <- get
   case rb > r-1 of
     True -> do
@@ -207,54 +235,103 @@ parallelAssemble md@(Model r mat) = do
       sMoveAbs (ra,r-1,0)
 
 parallelDisassemble :: Model -> BState ()
-parallelDisassemble md@(Model r mat) = do
+parallelDisassemble md@(Model r _) = do
   B{bRange=(ra,rb)} <- get
   case rb > r-1 of
     True -> do
       disassemble md
     _    -> do
-      sMoveAbs (rb-1,r-1,0)
+      sMoveAbs (rb-1,r-1,0) 
       b' <- fissionX
       let b'' = execState (parallelDisassemble md) b'
       disassemble md
       fusion b''
       sMoveAbs (ra,0,0)
 
+parallelReassemble :: Model -> Model -> BState ()
+parallelReassemble src@(Model r _) tgt = do
+  B{bRange=(ra,rb)} <- get
+  case rb > r-1 of
+    True -> do
+      reassemble src tgt
+    _    -> do
+      sMoveAbs (rb-1,0,0) -- 領域の端まで移動して
+      b' <- fissionX      -- 横の領域にボットを生成する
+      let b'' = execState (parallelReassemble src tgt) b'
+      reassemble src tgt
+      fusion b''          -- fusionするときにトレースもマージする
+      sMoveAbs (ra,r-1,0) -- 領域の端っこでfusionされるのを待つ
+
 assemble :: Model -> BState ()
 assemble (Model r mat) = do
   B{bRange=(ra,rb)} <- get
   sequence_
-    [ fillFloor i (maybe [] (filter (\(x,z)->ra<=x&&x<rb) . Set.toList) mset)
-    | i<-[0..r-2]
-    , let mset = IntMap.lookup i mat
+    [ fillFloor (x,y,z) 
+    | y <- [0..r-2]
+    , let mset = IntMap.lookup y mat
+    , (x,z) <- maybe [] (filter (\(x,z)->ra<=x&&x<rb) . Set.toList) mset
     ]
-  sMoveAbs (ra,r-1,0)
+  sMoveAbs (ra,r-1,0) -- 領域の端っこでfusionされるのを待つ
 
 disassemble :: Model -> BState ()
 disassemble (Model r mat) = do
   B{bRange=(ra,rb)} <- get
   sequence_
-    [ voidFloor i (maybe [] (filter (\(x,z)->ra<=x&&x<rb) . Set.toList) mset)
-    | i<-[r-1,r-2..0]
-    , let mset = IntMap.lookup i mat
+    [ voidFloor (x,y,z)
+    | y <- [r-1,r-2..0]
+    , let mset = IntMap.lookup y mat
+    , (x,z) <- maybe [] (filter (\(x,z)->ra<=x&&x<rb) . Set.toList) mset
     ]
-  sMoveAbs (ra,0,0)
+  sMoveAbs (ra,0,0) -- 領域の端っこでfusionされるのを待つ
 
-fillFloor :: Int -> [(Int,Int)] -> BState ()
-fillFloor fno xs =
-  mapM_ f xs
+reassemble :: Model -> Model -> BState ()
+reassemble (Model r srcmat) (Model _ tgtmat) = do
+  B{bRange=(ra,rb)} <- get
+  -- とにかくz軸方向を前後しながら動くように順番を調整する
+  let ls = zip (concat (repeat [True,False]))
+        [ (x, y, srcs, tgts)
+        | y <- [0..r-2]
+        , x <- [ra..rb-1]
+        , let srcs = maybe [] (filter ((==x).fst) . Set.toList) (IntMap.lookup y srcmat)
+        , let tgts = maybe [] (filter ((==x).fst) . Set.toList) (IntMap.lookup y tgtmat)
+        ]
+  sequence_ $ concat
+    [ [ sMoveAbs (x,y, if dir then 0 else r-1)] ++ -- z軸の端に移動する正規化(無駄な動きをするが分かりやすいので)
+      [ reconst ty dir (x,y,z)
+      -- sortの順番を (z, dir) の順で判定させる
+      | ((_x,z),ty) <- orderBy dir $ zip srcs (repeat (not dir, 'S')) ++ zip (map (adjustz dir) tgts) (repeat (dir, 'T'))
+      ]
+      ++ [sMoveAbs (x,y, if dir then r-1 else 0)] -- z軸のもう一方の端に移動する正規化(同上)
+    | (dir, (x, y, srcs, tgts)) <- ls
+    ]
+  sMoveAbs (ra,r-1,0) -- 領域の端っこでfusionされるのを待つ
   where
-    f :: (Int,Int) -> BState ()
-    f (x,z) = do
-      sMoveAbs (x,fno+1,z)
-      fillBottom
+    orderBy True  = sort
+    orderBy False = sortBy (flip compare)
 
-voidFloor :: Int -> [(Int,Int)] -> BState ()
-voidFloor fno xs =
-  mapM_ f xs
+    adjustz True  (i,j) = (i,j+1)
+    adjustz False (i,j) = (i,j-1)
+
+fillFloor :: (Int,Int,Int) -> BState ()
+fillFloor (x,y,z) = do
+  sMoveAbs (x,y+1,z)
+  cFill (0,-1,0)
+
+voidFloor :: (Int,Int,Int) -> BState ()
+voidFloor (x,y,z) = do
+  sMoveAbs (x,y+1,z)
+  cVoid (0,-1,0)
+
+reconst :: (Bool, Char) -> Bool -> (Int,Int,Int) -> BState ()
+reconst (_,'S') dir (x,y,z) = do
+  sMoveAbs (x,y,z+dz)
+  cVoid (0,0,-dz)
   where
-    f :: (Int,Int) -> BState ()
-    f (x,z) = do
-      sMoveAbs (x,fno+1,z)
-      voidBottom
+    dz = if dir then -1 else 1
+reconst (_,'T') dir (x,y,z) = do -- zは調整済みなので注意
+  sMoveAbs (x,y,z)
+  cFill (0,0,dz)
+  where
+    dz = if dir then -1 else 1
+
 
