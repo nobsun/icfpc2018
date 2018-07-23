95ff47110b61fbb9d1aa45bbe2e74939ed61f545
Kei Hibino <ex8k.hibino@gmail.com>
2018-07-23 13:17:16 +0900

    add problems and traces path.

--- src/Path.hs
+++ src/Path.hs
@@ -1,6 +1,15 @@
 module Path (
+  problems,
+  defaultTraces,
+
   submitExport,
   )  where
 
 submitExport :: FilePath
 submitExport = "/home/icfpc-export"
+
+problems :: FilePath
+problems = "/home/icfpc2018-data/problems/F"
+
+defaultTraces :: FilePath
+defaultTraces = "/home/icfpc2018-data/default/F"
