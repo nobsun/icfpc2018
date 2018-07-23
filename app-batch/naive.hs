89924ffda3fea2fc3c4e0dc4da33dc53466e7d49
Kei Hibino <ex8k.hibino@gmail.com>
2018-07-23 22:41:17 +0900

    add inverse mode.

--- app-batch/naive.hs
+++ app-batch/naive.hs
@@ -15,18 +15,21 @@ import Model (readModel)
 import qualified Model
 import NaiveBot (getAssembleTrace, getDisassembleTrace, getReassembleTrace)
 import TraceOptimizer (optimize)
+import TraceInverser (inverse)
 
 
 data Mode
   = NoOpt
   | Opt
   | TOpt
+  | Inv
   deriving (Eq, Show)
 
 descMode :: Mode -> String
 descMode NoOpt = "no-optimize"
 descMode Opt   = "optimize"
 descMode TOpt  = "optimize (may timeoute)"
+descMode Inv  =  "optimize (may timeoute, inversed disassemble)"
 
 limitAD :: Int
 limitAD = 4 * 60 * 1000 * 1000
@@ -42,6 +45,7 @@ assemble mode nbt _n tgt_ = do
       trs NoOpt  =  return (trs0,      False)
       trs Opt    =  return (optimized, False)
       trs TOpt   =  maybe (trs0, True) (\x -> (x, False)) <$> timeout limitAD (optimized `deepseq` return optimized)
+      trs Inv    =  maybe (trs0, True) (\x -> (x, False)) <$> timeout limitAD (optimized `deepseq` return optimized)
   (t, tout) <- trs mode
   writeTraceFile nbt t
   return tout
@@ -52,14 +56,16 @@ disassemble _mode nbt _n src_ = do
   let trs0 = getDisassembleTrace src
   writeTraceFile nbt trs0
   return False
-  -- バグで結果が不正なトレースになってしまう可能性があるので、disassembly とreassembly のoptimizeはやらない
-  --     optimized = optimize src (Model.emptyR src) trs0
-  --     trs NoOpt  =  return (trs0,      False)
-  --     trs Opt    =  return (optimized, False)
-  --     trs TOpt   =  maybe (trs0, True) (\x -> (x, False)) <$> timeout limitAD (optimized `deepseq` return optimized)
-  -- (t, tout) <- trs mode
-  -- writeTraceFile nbt t
-  -- return tout
+
+disassembleInv :: FilePath -> Int -> FilePath -> IO Bool
+disassembleInv nbt _n src_ = do
+  tgt <- readModel $ Path.problems </> src_
+  let trs0 = getAssembleTrace tgt  -- run as assemble in inverse mode
+      optimized = inverse $ optimize (Model.emptyR tgt) tgt trs0
+      trs       =  maybe (inverse trs0, True) (\x -> (x, False)) <$> timeout limitAD (optimized `deepseq` return optimized)
+  (t, tout) <- trs
+  writeTraceFile nbt t
+  return tout
 
 reassemble :: Mode -> FilePath -> Int -> FilePath -> FilePath -> IO Bool
 reassemble _mode nbt _n src_ tgt_ = do
@@ -68,14 +74,6 @@ reassemble _mode nbt _n src_ tgt_ = do
   let trs0 = getReassembleTrace src tgt
   writeTraceFile nbt trs0
   return False
-  -- バグで結果が不正なトレースになってしまう可能性があるので、disassembly とreassembly のoptimizeはやらない
-  --     optimized = optimize src tgt trs0
-  --     trs NoOpt  =  return (trs0,      False)
-  --     trs Opt    =  return (optimized, False)
-  --     trs TOpt   =  maybe (trs0, True) (\x -> (x, False)) <$> timeout limitR  (optimized `deepseq` return optimized)
-  -- (t, tout) <- trs mode
-  -- writeTraceFile nbt t
-  -- return tout
 
 run :: (String -> IO ())
     -> FilePath
@@ -85,10 +83,12 @@ run :: (String -> IO ())
 run putLog dst mode pf = do
   let nbtPath = dst </> ProbSet.traceFile pf
       label = nbtPath
+      disas Inv   = disassembleInv
+      disas m     = disassemble m
   putLog $ "running: " ++ label
   tout <- runProblemFile
     (assemble    mode nbtPath)
-    (disassemble mode nbtPath)
+    (disas       mode nbtPath)
     (reassemble  mode nbtPath)
     pf
   when tout . putLog $ "timeout: " ++ label
@@ -105,7 +105,8 @@ runAll dst mode = do
   n <- getNumCapabilities
   let orderedP NoOpt  =  ProbSet.problemsL
       orderedP Opt    =  reverse ProbSet.problemsL  -- opt は高コストなので小さい順
-      orderedP TOpt   =  reverse ProbSet.problemsL  -- opt は高コストなので小さい順
+      orderedP TOpt   =  reverse ProbSet.problemsL
+      orderedP Inv    =  reverse ProbSet.problemsL
   concurrentIO (n - 1) $ map (run putLog dst mode) $ orderedP mode
 
 main :: IO ()
@@ -114,6 +115,7 @@ main = do
   (dst, doOpt) <- case args of
     d:"opt":_   ->  return (d, Opt)
     d:"topt":_  ->  return (d, TOpt)
+    d:"inv":_   ->  return (d, Inv)
     d:_         ->  return (d, NoOpt)
     _   -> fail "DEST_DIRECTORY is required."
   runAll dst doOpt
