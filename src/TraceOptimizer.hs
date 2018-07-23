4a860e15515179bdf18de76a567e4f89d9967e79
Masahiro Sakai <masahiro.sakai@gmail.com>
2018-07-23 10:27:25 +0900

    clean up TraceOptimizer a little

--- src/TraceOptimizer.hs
+++ src/TraceOptimizer.hs
@@ -14,40 +14,46 @@ optimize ini fini trace = concat [IntMap.elems cmds | cmds <- loop Low transitio
   where
     transitions :: [(Bool, IntMap Command, Bool)]
     transitions = [(pre, cmds, post) | ((pre,cmds),post) <- zip xs (tail (map fst xs) ++ [True])]
-
-    xs :: [(Bool, IntMap Command)]
-    xs = f (initialStateForReassembly ini fini trace)
       where
-        f :: SystemState -> [(Bool, IntMap Command)]
-        f s
-          | IntMap.null (stBots s) = []
-          | otherwise = 
-              ( isGrounded (stMatrix s)
-              , IntMap.fromAscList $ [(bid, cmd) | ((bid,_),cmd) <- zip (IntMap.toAscList (stBots s)) (take n (stTrace s))]
-              ) : f (execOneStep s)
+        xs :: [(Bool, IntMap Command)]
+        xs = f (initialStateForReassembly ini fini trace)
           where
-            n = IntMap.size (stBots s)
+            f :: SystemState -> [(Bool, IntMap Command)]
+            f s
+              | IntMap.null (stBots s) = []
+              | otherwise = 
+                  ( isGrounded (stMatrix s)
+                  , IntMap.fromAscList $ [(bid, cmd) | ((bid,_),cmd) <- zip (IntMap.toAscList (stBots s)) (take n (stTrace s))]
+                  ) : f (execOneStep s)
+              where
+                n = IntMap.size (stBots s)
 
     loop :: Harmonics -> [(Bool, IntMap Command, Bool)] -> [IntMap Command]
     loop _ [] = []
     loop Low ((_True, cmds, True) : ts)
       | Flip `elem` IntMap.elems cmds = replaceFlipWithWait cmds : loop Low ts
       | otherwise = cmds : loop Low ts
-    loop Low tts@((_True, cmds, False) : ts)
-      | Flip `elem` IntMap.elems cmds = cmds : loop High ts
-      | Wait `elem` IntMap.elems cmds = replaceWait Flip cmds : loop High ts
-      | otherwise = flipOnly cmds : loop High tts
-    loop High tts@((True, cmds, True) : ts)
-      | Flip `elem` IntMap.elems cmds = cmds : loop Low ts
-      | Wait `elem` IntMap.elems cmds = replaceWait Flip cmds : loop Low ts
-      | otherwise = flipOnly cmds : loop Low tts
-    loop High ((False, cmds, True) : ts)
-      | Flip `elem` IntMap.elems cmds = cmds : loop Low ts
-      | Wait `elem` IntMap.elems cmds = replaceWait Flip cmds : loop Low ts
-      | otherwise = cmds : loop High ts
+    loop Low tts@((_True, cmds, False) : ts) =
+      case flipInplace cmds of
+        Just cmds' -> cmds' : loop High ts
+        Nothing -> flipOnly cmds : loop High tts
+    loop High tts@((True, cmds, True) : ts) =
+      case flipInplace cmds of
+        Just cmds' -> cmds' : loop Low ts
+        Nothing -> flipOnly cmds : loop Low tts -- 現在がgroundedなので先にlowにできる
+    loop High ((False, cmds, True) : ts) =
+      case flipInplace cmds of
+        Just cmds' -> cmds' : loop Low ts
+        Nothing -> cmds : loop High ts -- 現在がgroundedでないので先にlowにすることはできない
     loop harmonics ((_preGrounded,cmds,_postGrounded) : ts)
       | Flip `elem` IntMap.elems cmds = cmds : loop (flipHarmonics harmonics) ts
       | otherwise = cmds : loop harmonics ts
+
+    flipInplace :: IntMap Command -> Maybe (IntMap Command)
+    flipInplace cmds
+      | Flip `elem` IntMap.elems cmds = return cmds
+      | Wait `elem` IntMap.elems cmds = return $ replaceWait Flip cmds
+      | otherwise = Nothing
 
     replaceWait :: Command -> IntMap Command -> IntMap Command
     replaceWait cmd cmds =
