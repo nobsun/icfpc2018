827105ec51bc8d86a9316d9412cdc6a12c4988b0
Kei Hibino <ex8k.hibino@gmail.com>
2018-07-23 18:57:16 +0900

    add timeout message.

--- app-traces/naive.hs
+++ app-traces/naive.hs
@@ -1,5 +1,5 @@
 import Control.Applicative ((*>))
-import Control.Monad (forever, unless, void)
+import Control.Monad (forever, when, unless, void)
 import Control.Concurrent (forkIO, getNumCapabilities)
 import Control.Concurrent.Chan (newChan, readChan, writeChan)
 import Control.DeepSeq (deepseq)
@@ -58,36 +58,42 @@ limitAD = 4 * 60 * 1000 * 1000
 limitR :: Int
 limitR = 6 * 60 * 1000 * 1000
 
-assemble :: Mode -> FilePath -> Int -> FilePath -> IO ()
+assemble :: Mode -> FilePath -> Int -> FilePath -> IO Bool
 assemble mode nbt _n tgt_ = do
   tgt <- readModel $ Path.problems </> tgt_
   let trs0 = getAssembleTrace tgt
       optimized = optimize (Model.emptyR tgt) tgt trs0
-      trs NoOpt  =  return trs0
-      trs Opt    =  return optimized
-      trs TOpt   =  maybe trs0 id <$> timeout limitAD (optimized `deepseq` return optimized)
-  writeTraceFile nbt =<< trs mode
+      trs NoOpt  =  return (trs0,      False)
+      trs Opt    =  return (optimized, False)
+      trs TOpt   =  maybe (trs0, True) (\x -> (x, False)) <$> timeout limitAD (optimized `deepseq` return optimized)
+  (t, tout) <- trs mode
+  writeTraceFile nbt t
+  return tout
 
-disassemble :: Mode -> FilePath -> Int -> FilePath -> IO ()
+disassemble :: Mode -> FilePath -> Int -> FilePath -> IO Bool
 disassemble mode nbt _n src_ = do
   src <- readModel $ Path.problems </> src_
   let trs0 = getDisassembleTrace src
       optimized = optimize src (Model.emptyR src) trs0
-      trs NoOpt  =  return trs0
-      trs Opt    =  return optimized
-      trs TOpt   =  maybe trs0 id <$> timeout limitAD (optimized `deepseq` return optimized)
-  writeTraceFile nbt =<< trs mode
+      trs NoOpt  =  return (trs0,      False)
+      trs Opt    =  return (optimized, False)
+      trs TOpt   =  maybe (trs0, True) (\x -> (x, False)) <$> timeout limitAD (optimized `deepseq` return optimized)
+  (t, tout) <- trs mode
+  writeTraceFile nbt t
+  return tout
 
-reassemble :: Mode -> FilePath -> Int -> FilePath -> FilePath -> IO ()
+reassemble :: Mode -> FilePath -> Int -> FilePath -> FilePath -> IO Bool
 reassemble mode nbt _n src_ tgt_ = do
   src <- readModel $ Path.problems </> src_
   tgt <- readModel $ Path.problems </> tgt_
   let trs0 = getReassembleTrace src tgt
       optimized = optimize src tgt trs0
-      trs NoOpt  =  return trs0
-      trs Opt    =  return optimized
-      trs TOpt   =  maybe trs0 id <$> timeout limitR  (optimized `deepseq` return optimized)
-  writeTraceFile nbt =<< trs mode
+      trs NoOpt  =  return (trs0,      False)
+      trs Opt    =  return (optimized, False)
+      trs TOpt   =  maybe (trs0, True) (\x -> (x, False)) <$> timeout limitR  (optimized `deepseq` return optimized)
+  (t, tout) <- trs mode
+  writeTraceFile nbt t
+  return tout
 
 run :: (String -> IO ())
     -> FilePath
@@ -98,11 +104,12 @@ run putLog dst mode pf = do
   let nbtPath = dst </> ProbSet.traceFile pf
       label = nbtPath
   putLog $ "running: " ++ label
-  runProblemFile
+  tout <- runProblemFile
     (assemble    mode nbtPath)
     (disassemble mode nbtPath)
     (reassemble  mode nbtPath)
     pf
+  when tout . putLog $ "timeout: " ++ label
   putLog $ "done   : " ++ label
 
 runAll :: FilePath -> Mode -> IO ()
